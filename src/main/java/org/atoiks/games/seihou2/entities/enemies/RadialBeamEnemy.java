198488cfdaf9f5c1ec3e1a1206c49bbf84dce3aa
Paul T <plankp@outlook.com>
2018-02-05 09:03:46 -0500

    Radial based enemy templates

---
+++ src/main/java/org/atoiks/games/seihou2/entities/enemies/RadialBeamEnemy.java
@@ -0,0 +1,56 @@
+package org.atoiks.games.seihou2.entities.enemies;
+
+import se.tube42.lib.tweeny.Item;
+
+import org.atoiks.games.seihou2.entities.Beam;
+
+public final class RadialBeamEnemy extends AbstractEnemy {
+
+    private static final long serialVersionUID = 7329566493126725388L;
+
+    private final int score;
+    private final int intervals;
+    private final float thickness;
+    private final float length;
+    private final float fireInterval;
+    private final float delay;
+    private final float initialAngle;
+    private final float anglePerInterval;
+    private final float speed;
+
+    private float time;
+    private int bulletId;
+
+    public RadialBeamEnemy(int hp, int score, Item tweenInfo, final float fireInterval, boolean immediateFire, float delay, float initialAngle, int intervals, float anglePerInterval, float thickness, int length, float speed) {
+        super(hp, tweenInfo);
+        this.score = score;
+        this.thickness = thickness;
+        this.length = length;
+        this.initialAngle = initialAngle;
+        this.anglePerInterval = anglePerInterval;
+        this.speed = speed;
+        this.intervals = intervals;
+        this.delay = delay;
+        this.fireInterval = fireInterval;
+        if (!immediateFire) {
+            bulletId = intervals;
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        time += dt;
+        if (bulletId >= intervals) {
+            if (time >= fireInterval) bulletId = 0;
+        } else if (time > delay) {
+            game.addEnemyBullet(new Beam(getX(), getY(), thickness, length, initialAngle + bulletId * anglePerInterval, speed));
+            ++bulletId;
+            time = 0;
+        }
+    }
+
+	@Override
+	public int getScore() {
+		return score;
+	}
+}
---
+++ src/main/java/org/atoiks/games/seihou2/entities/enemies/RadialPointEnemy.java
@@ -0,0 +1,53 @@
+package org.atoiks.games.seihou2.entities.enemies;
+
+import org.atoiks.games.seihou2.entities.PointBullet;
+
+import se.tube42.lib.tweeny.Item;
+
+public final class RadialPointEnemy extends AbstractEnemy {
+
+    private static final long serialVersionUID = 1L;
+
+    private final int score;
+    private final int intervals;
+    private final float speed;
+    private final float radius;
+    private final float fireInterval;
+    private final float delay;
+    private final float initialAngle;
+    private final float anglePerInterval;
+
+    private float time;
+    private int bulletId;
+
+    public RadialPointEnemy(int hp, int score, Item tweenInfo, final float fireInterval, boolean immediateFire, float delay, float initialAngle, int intervals, float anglePerInterval, float radius, float speed) {
+        super(hp, tweenInfo);
+        this.score = score;
+        this.intervals = intervals;
+        this.speed = speed;
+        this.radius = radius;
+        this.delay = delay;
+        this.fireInterval = fireInterval;
+        if (immediateFire) {
+            bulletId = intervals;
+        }
+        this.initialAngle = initialAngle;
+        this.anglePerInterval = anglePerInterval;
+    }
+
+    @Override
+    public void update(float dt) {
+        time += dt;
+        if (bulletId >= intervals) {
+            if (time >= fireInterval) bulletId = 0;
+        } else if (time > delay) {
+            final float angle = initialAngle + bulletId * anglePerInterval;
+            game.addEnemyBullet(new PointBullet(getX(), getY(), radius, (float) (speed * Math.cos(angle)), (float) (speed * Math.sin(angle))));
+        }
+    }
+
+	@Override
+	public int getScore() {
+		return score;
+	}
+}
