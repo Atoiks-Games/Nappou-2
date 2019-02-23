9d7865793a30b90a5cbb38075d6825405543fb54
Paul Teng <plankp@outlook.com>
2019-02-23 09:14:34 -0500

    Use inheritance to simplfy WiggleBullet

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/bullet/WiggleBullet.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/bullet/WiggleBullet.java
@@ -20,31 +20,18 @@ package org.atoiks.games.nappou2.entities.bullet;
 
 import org.atoiks.games.framework2d.IGraphics;
 
-import org.atoiks.games.nappou2.entities.IBullet;
-
 import static org.atoiks.games.nappou2.Utils.fastCircleCollision;
 
-public class WiggleBullet extends IBullet {
+public class WiggleBullet extends PointBullet {
 
     private static final long serialVersionUID = 3928242215L;
 
-    private static final int SCREEN_EDGE_BUFFER = 16;
-
-    protected float x, y, r;
-    protected float dx, dy;
     protected float m, s, time;
     protected boolean d;
 
-    protected WiggleBullet(float x, float y, float r) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
-    }
-
     public WiggleBullet(float x, float y, float r, float dx, float dy, boolean d, float m, float s) {
-        this(x, y, r);
-        this.dx = dx;
-        this.dy = dy;
+        super(x, y, r, dx, dy);
+
         this.d = d;
         this.m = m;
         this.s = s;
@@ -52,53 +39,15 @@ public class WiggleBullet extends IBullet {
     }
 
     @Override
-    public void translate(float dx, float dy) {
-        this.x += dx;
-        this.y += dy;
-    }
-
-    @Override
-    public void render(final IGraphics g) {
-        // Can change this to using textures later
-        g.setColor(color);
-        // x, y are the center of the bullet
-        g.drawOval(x - r, y - r, x + r, y + r);
-    }
-
-    @Override
     public void update(final float dt) {
         this.time += dt;
 
-        if(this.d == true){
+        if (this.d) {
             this.x += dt * (this.dx + this.m * Math.cos(this.s * this.time));
             this.y += this.dy * dt;
         } else {
             this.x += this.dx * dt;
             this.y += dt * (this.dy + this.m * Math.cos(this.s * this.time));
         }
-
-    }
-
-    @Override
-    public float getX() {
-        return this.x;
-    }
-
-    @Override
-    public float getY() {
-        return this.y;
-    }
-
-    @Override
-    public boolean collidesWith(final float x1, final float y1, final float r1) {
-        return fastCircleCollision(x, y, r, x1, y1, r1);
-    }
-
-    @Override
-    public boolean isOutOfScreen(final int w, final int h) {
-        return (x + r < -SCREEN_EDGE_BUFFER)
-            || (x - r > w + SCREEN_EDGE_BUFFER)
-            || (y + r < -SCREEN_EDGE_BUFFER)
-            || (y - r > h + SCREEN_EDGE_BUFFER);
     }
 }
