2f109ace10e4d732a0c0bc818da7044f2c7b667a
PerenDarksbane <ajbimm@gmail.com>
2019-03-02 15:17:24 -0500

    Giving Credit

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/scenes/CreditsScene.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/scenes/CreditsScene.java
@@ -44,6 +44,9 @@ public final class CreditsScene extends CenteringScene {
         g.setFont(TitleScene.OPTION_FONT);
         g.drawString("Thanks folks!", 10, 30);
 
+
+        g.drawString("Alexander Bimm, Jeongmin Lee, Paul Teng, Shelby Elder, Sam Markowitz", 10, 50);
+
         g.setFont(SANS_FONT);
         g.drawString("Hit Escape or Enter to return to title screen", 14, 580);
     }
