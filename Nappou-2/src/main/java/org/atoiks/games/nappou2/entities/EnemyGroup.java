0b4619eac537007147954285fa6885a8aee8fb3f
Paul Teng <plankp@outlook.com>
2019-03-27 08:51:39 -0400

    Remove EnemyGroup from IEnemy

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/EnemyGroup.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/EnemyGroup.java
@@ -22,72 +22,16 @@ import java.util.stream.Stream;
 import java.util.function.Supplier;
 import java.util.function.IntFunction;
 
-import org.atoiks.games.framework2d.IGraphics;
-
-import org.atoiks.games.nappou2.graphics.NullEnemyRenderer;
-
-public abstract class EnemyGroup implements IEnemy {
+public abstract class EnemyGroup {
 
     private static final long serialVersionUID = 823469624677L;
 
     protected Game game;
 
-    @Override
-    public final boolean isDead() {
-        // A spawner cannot die
-        return false;
-    }
+    public abstract void update(float dt);
 
-    @Override
-    public final int changeHp(int delta) {
-        return 1;
-    }
-
-    @Override
-    public final void drift(float dx, float dy) {
-        // do nothing
-    }
-
-    @Override
-    public final void render(IGraphics g) {
-        // do nothing
-    }
-
-    @Override
     public final void attachGame(Game game) {
         this.game = game;
-    }
-
-    @Override
-    public final float getX() {
-        return Integer.MIN_VALUE / 2;
-    }
-
-    @Override
-    public final float getY() {
-        return Integer.MIN_VALUE / 2;
-    }
-
-    @Override
-    public final float getR() {
-        return -1;
-    }
-
-    @Override
-    public final boolean collidesWith(float x, float y, float r) {
-        // Never collides
-        return false;
-    }
-
-    @Override
-    public final int getScore() {
-        // You cannot destroy enemy groups by attacking it...
-        return 0;
-    }
-
-    @Override
-    public final boolean isOutOfScreen(int width, int height) {
-        return isDoneSpawning();
     }
 
     public abstract boolean isDoneSpawning();
