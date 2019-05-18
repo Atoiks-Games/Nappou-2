f0682e0d43c923105063bafff4d8e2e888918588
Paul Teng <plankp@outlook.com>
2019-05-18 16:12:39 -0400

    Convert single shot enemy

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/enemy/SingleShotEnemy.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/enemy/SingleShotEnemy.java
@@ -20,41 +20,30 @@ package org.atoiks.games.nappou2.entities.enemy;
 
 import org.atoiks.games.nappou2.entities.bullet.PointBullet;
 
-public final class SingleShotEnemy extends ManualEnemy {
+public final class SingleShotEnemy extends FireGateEnemy {
 
-    private static final long serialVersionUID = 5619264522L;
-
-    private float time;
-    private boolean fireGate;
+    private static final long serialVersionUID = -3483536584027126124L;
 
     private final int invSign;
 
     public SingleShotEnemy(int hp, float x, float y, float r, boolean inverted) {
-        super(hp, x, y, r);
+        super(hp, x, y, r, 6);
         this.invSign = inverted ? -1 : 1;
-    }
-
-    @Override
-    public void update(float dt) {
-        time += dt;
-
-        setY(getY() + invSign * 300 * dt);
-
-        if (!fireGate && Math.cos(6 * time) < 0.5) {
-            fireGate = true;
-        }
-
-        if (fireGate && Math.cos(6 * time) > 0.5) {
-            fireGate = false;
-            final float x = getX();
-            final float y = getY();
-            final double angle = Math.atan2(game.player.getY() - y, game.player.getX() - x);
-            game.addEnemyBullet(new PointBullet(x, y, 2, 1000 * (float) Math.cos(angle), 1000 * (float) Math.sin(angle)));
-        }
     }
 
     @Override
     public int getScore() {
         return 1;
+    }
+
+    @Override
+    protected void customUpdate(float dt) {
+        y += invSign * 300 * dt;
+    }
+
+    @Override
+    protected void customFireAction(float dt) {
+        final double angle = Math.atan2(game.player.getY() - y, game.player.getX() - x);
+        game.addEnemyBullet(new PointBullet(x, y, 2, 1000 * (float) Math.cos(angle), 1000 * (float) Math.sin(angle)));
     }
 }
