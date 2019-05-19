135e213581e86683ccd26ba4a07b5ad11b4cfbc3
Paul Teng <plankp@outlook.com>
2019-05-19 11:51:21 -0400

    Migrate existing bullet patterns

---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/attack/SingleShot.java
@@ -0,0 +1,38 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.entities.attack;
+
+import org.atoiks.games.nappou2.entities.Game;
+import org.atoiks.games.nappou2.entities.IEnemy;
+import org.atoiks.games.nappou2.entities.IUpdateListener;
+
+import org.atoiks.games.nappou2.entities.bullet.PointBullet;
+
+public final class SingleShot implements IUpdateListener {
+
+    @Override
+    public void onFireUpdate(IEnemy enemy, float dt) {
+        final Game game = enemy.getAssocGame();
+        final float x = enemy.getX();
+        final float y = enemy.getY();
+
+        final double angle = Math.atan2(game.player.getY() - y, game.player.getX() - x);
+        game.addEnemyBullet(new PointBullet(x, y, 2, 1000 * (float) Math.cos(angle), 1000 * (float) Math.sin(angle)));
+    }
+}
---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/attack/TrackingPattern.java
@@ -0,0 +1,66 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.entities.attack;
+
+import org.atoiks.games.nappou2.entities.Game;
+import org.atoiks.games.nappou2.entities.IEnemy;
+import org.atoiks.games.nappou2.entities.IUpdateListener;
+
+import org.atoiks.games.nappou2.entities.bullet.factory.BulletFactory;
+
+public final class TrackingPattern implements IUpdateListener {
+
+    private final BulletFactory factory;
+
+    private final float fireInterval;
+    private final float delay;
+    private final float[] angleOffsets;
+
+    private float time;
+    private int bulletId;
+
+    public TrackingPattern(float fireInterval, boolean immediateFire, float delay, float[] angleOffsets, BulletFactory factory) {
+        this.factory = factory;
+
+        this.fireInterval = fireInterval;
+        this.delay = delay;
+        this.angleOffsets = angleOffsets;
+
+        if (!immediateFire) {
+            bulletId = angleOffsets.length;
+        }
+    }
+
+    @Override
+    public void onFireUpdate(IEnemy enemy, float dt) {
+        time += dt;
+        if (bulletId >= angleOffsets.length) {
+            if (time >= fireInterval) bulletId = 0;
+        } else if (time > delay) {
+            final Game game = enemy.getAssocGame();
+            final float x = enemy.getX();
+            final float y = enemy.getY();
+
+            final float angle = (float) Math.atan2(game.player.getY() - y, game.player.getX() - x) + angleOffsets[bulletId];
+            game.addEnemyBullet(factory.createBullet(x, y, angle));
+            ++bulletId;
+            time = 0;
+        }
+    }
+}
