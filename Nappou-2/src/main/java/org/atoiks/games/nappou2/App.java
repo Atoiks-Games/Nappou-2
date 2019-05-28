ec29af6f9407db6ff5cb90589e6887e541af58ca
Paul Teng <plankp@outlook.com>
2019-05-28 08:12:02 -0400

    Load scene with SceneManager

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/App.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/App.java
@@ -26,6 +26,7 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 
 import org.atoiks.games.framework2d.FrameInfo;
+import org.atoiks.games.framework2d.SceneManager;
 import org.atoiks.games.framework2d.ResourceManager;
 
 import org.atoiks.games.framework2d.java2d.Frame;
@@ -54,15 +55,17 @@ public class App {
                 .setTitle("Atoiks Games - Void Walker")
                 .setResizable(false)
                 .setSize(900, 600)
-                .setFps(120.0f)
-                .setFirstScene("LoadingScene")
-                .setScenes(new LoadingScene(),
-                        new SaveHighscoreScene(), new TitleScene(), new TutorialScene(),
-                        new ScoreScene(), new CreditsScene(), new ConfigScene(),
-                        new DiffOptionScene(), new ShieldOptionScene(),
-                        new LevelOneScene(), new LevelTwoScene());
+                .setFps(120.0f);
+
+        SceneManager.loadScenes(new LoadingScene(),
+                new SaveHighscoreScene(), new TitleScene(), new TutorialScene(),
+                new ScoreScene(), new CreditsScene(), new ConfigScene(),
+                new DiffOptionScene(), new ShieldOptionScene(),
+                new LevelOneScene(), new LevelTwoScene());
+
         final Frame frame = new Frame(info);
         try {
+            SceneManager.switchToScene("LoadingScene");
             frame.init();
             frame.loop();
         } finally {
