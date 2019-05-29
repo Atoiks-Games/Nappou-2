5fd0ca01ddf727a9a8e80141c02de167c64e1375
Paul Teng <plankp@outlook.com>
2019-05-29 16:00:35 -0400

    Fix incorrect result in equals

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/Vector2.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/Vector2.java
@@ -102,7 +102,7 @@ public final class Vector2 {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == this) return false;
+        if (obj == this) return true;
         if (obj == null) return false;
         if (obj.getClass() != this.getClass()) return false;
 
