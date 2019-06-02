a11e35008e479b2e28d3702d38784b2400ff6871
Paul Teng <plankp@outlook.com>
2019-06-01 21:41:18 -0400

    Create enemy bullet spawner

---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/spawner/EnemyBulletSpawner.java
@@ -0,0 +1,51 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.spawner;
+
+import org.atoiks.games.nappou2.entities.Game;
+
+import org.atoiks.games.nappou2.entities.bullet.IBullet;
+
+public final class EnemyBulletSpawner implements ISpawner {
+
+    private final IBullet[] bullets;
+    private final float delay;
+
+    private float time;
+    private int index;
+
+    public EnemyBulletSpawner(float delay, IBullet... bullets) {
+        this.delay = delay;
+        this.bullets = bullets;
+    }
+
+    @Override
+    public void onUpdate(final Game game, float dt) {
+        while (index < bullets.length && (time += dt) >= delay) {
+            time -= delay;
+            final IBullet bullet = bullets[index++];
+            if (bullet != null) game.addEnemyBullet(bullet);
+        }
+    }
+
+    @Override
+    public boolean isDoneSpawning() {
+        return index >= bullets.length;
+    }
+}
