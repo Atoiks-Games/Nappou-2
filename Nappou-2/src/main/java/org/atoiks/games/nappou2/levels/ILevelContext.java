d447ac90cd9b44e1d3227d504da3c37de2e42b17
Paul Teng <plankp@outlook.com>
2019-06-02 16:59:08 -0400

    Create level state and context

---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/levels/ILevelContext.java
@@ -0,0 +1,51 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.levels;
+
+import org.atoiks.games.framework2d.IGraphics;
+
+import org.atoiks.games.nappou2.Drifter;
+import org.atoiks.games.nappou2.Difficulty;
+
+import org.atoiks.games.nappou2.entities.Game;
+import org.atoiks.games.nappou2.entities.Message;
+
+public interface ILevelContext {
+
+    public void setState(final ILevelState nextState);
+
+    // Create an enum containing all the possible level ids?
+    public void setLevelId(int id);
+    public Game getGame();
+    public Drifter getDrifter();
+    public Difficulty getDifficulty();
+
+    public void shouldSkipPlayerUpdate(boolean flag);
+
+    public void clearMessage();
+    public void displayMessage(Message msg);
+
+    public default void disableDamage() {
+        getGame().player.setIgnoreHpChange(true);
+    }
+
+    public default void enableDamage() {
+        getGame().player.setIgnoreHpChange(false);
+    }
+}
---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/levels/ILevelState.java
@@ -0,0 +1,36 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.levels;
+
+import org.atoiks.games.framework2d.IGraphics;
+
+public interface ILevelState {
+
+    // Maybe pass in the previous state?
+    public default void enter(ILevelContext ctx) {
+    }
+
+    public default void exit() {
+    }
+
+    public default void renderBackground(final IGraphics g) {
+    }
+
+    public void updateLevel(ILevelContext ctx, float dt);
+}
