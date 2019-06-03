009cfcdd534e312a0e9380c34861b4d2cd2f1d19
Paul Teng <plankp@outlook.com>
2019-06-03 06:11:59 -0400

    Extract dialog state

---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/levels/AbstractDialogState.java
@@ -0,0 +1,71 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.levels;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+
+import java.util.Iterator;
+
+import javax.sound.sampled.Clip;
+
+import org.atoiks.games.framework2d.Input;
+import org.atoiks.games.framework2d.ResourceManager;
+
+import org.atoiks.games.nappou2.GameConfig;
+
+import org.atoiks.games.nappou2.entities.Message;
+
+public abstract class AbstractDialogState implements ILevelState, Iterator<Message> {
+
+    private final ILevelState nextState;
+
+    private boolean fetchMessage;
+
+    public AbstractDialogState(ILevelState nextState) {
+        this.nextState = nextState;
+    }
+
+    @Override
+    public void enter(final ILevelContext ctx) {
+        ctx.disableDamage();
+        ctx.shouldSkipPlayerUpdate(true);
+        ctx.getGame().clearBullets();
+
+        this.fetchMessage = true;
+    }
+
+    @Override
+    public void updateLevel(final ILevelContext ctx, final float dt) {
+        if (this.fetchMessage) {
+            if (this.hasNext()) {
+                this.fetchMessage = false;
+                ctx.displayMessage(this.next());
+            } else {
+                ctx.clearMessage();
+                ctx.setState(nextState);
+                return;
+            }
+        }
+
+        if (Input.isKeyPressed(KeyEvent.VK_ENTER)) {
+            this.fetchMessage = true;
+        }
+    }
+}
