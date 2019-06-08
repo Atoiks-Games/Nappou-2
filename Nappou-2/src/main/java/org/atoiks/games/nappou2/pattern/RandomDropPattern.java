4728a539863ee9cda72f7b0a34b862d5ec3279f1
Paul Teng <plankp@outlook.com>
2019-06-08 14:07:41 -0400

    Add optional lower bound on RandomDropPattern

--- Nappou-2/src/main/java/org/atoiks/games/nappou2/pattern/RandomDropPattern.java
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/pattern/RandomDropPattern.java
@@ -26,16 +26,23 @@ import java.util.Random;
 
 public final class RandomDropPattern implements IAttackPattern {
 
-    private final BulletFactory factory;
     public static final Random rnd = new Random();
-    private int limit;
+
+    private final BulletFactory factory;
+    private final float scale;
+    private final float lower;
     private float fire;
     private float time = 0;
 
-    public RandomDropPattern(int limit, BulletFactory factory) {
+    public RandomDropPattern(float limit, BulletFactory factory) {
+        this(0, limit, factory);
+    }
+
+    public RandomDropPattern(float lo, float hi, BulletFactory factory) {
         this.factory = factory;
-        this.limit = limit;
-        this.fire = rnd.nextFloat() * limit;
+        this.scale = Math.abs(hi - lo);
+        this.lower = Math.min(lo, hi);
+        this.recalcFireTime();
     }
 
     @Override
@@ -44,9 +51,13 @@ public final class RandomDropPattern implements IAttackPattern {
 
         if (this.time > this.fire) {
             this.time = 0;
-            this.fire = rnd.nextFloat() * this.limit;
+            this.recalcFireTime();
             final Game game = enemy.getAssocGame();
             game.addEnemyBullet(factory.createBullet(enemy.getPosition(), (float) (Math.PI / 2)));
         }
+    }
+
+    private void recalcFireTime() {
+        this.fire = this.lower + rnd.nextFloat() * this.scale;
     }
 }
