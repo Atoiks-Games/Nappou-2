1264a58055676f8c6321e89b5608c4c42b2197fe
Paul Teng <plankp@outlook.com>
2019-07-04 06:07:47 -0400

    Add default restore class

---
+++ Nappou-2/src/main/java/org/atoiks/games/nappou2/entities/DefaultRestoreData.java
@@ -0,0 +1,52 @@
+/**
+ *  Nappou-2
+ *  Copyright (C) 2017-2019  Atoiks-Games <atoiks-games@outlook.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.atoiks.games.nappou2.entities;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Externalizable;
+
+public final class DefaultRestoreData implements Externalizable {
+
+    private static final long serialVersionUID = 7388399896699153942L;
+
+    private int hp;
+    private int score;
+
+    public void restore(Game game) {
+        game.setScore(score);
+        game.player.setHp(hp);
+    }
+
+    public void fetch(Game game) {
+        this.score = game.getScore();
+        this.hp = game.player.getHp();
+    }
+
+    public void readExternal(ObjectInput s) throws IOException, ClassNotFoundException {
+        this.hp = s.readInt();
+        this.score = s.readInt();
+    }
+
+    public void writeExternal(ObjectOutput s) throws IOException {
+        s.writeInt(this.hp);
+        s.writeInt(this.score);
+    }
+}
